69590c32639e2f26a3f6033214289eca168dbeb5
Michiel <michiel@kauwatjoe.com>
2021-01-19 10:15:56 +0100

    3.9 venv.

--- venv.sh
+++ venv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python3 -m venv myvenv
+#python3 -m venv myvenv
+python3.9 -m venv build_env
 source myvenv/bin/activate
-python3 -m pip install --upgrade pip
-pip install -r requirements.txt
+python3.9 -m pip install --upgrade pip
+pip3.9 install -r requirements.txt
